2a6336caa58b7e9f6792a032a3f7e48348000089
Khalid Saeed <KhalidSQM@gmail.com>
2024-03-27 02:34:08 +0200

    git article

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert("Hello World");
+let element = document.getElementById("container");
+
+function changBackground() {
+  element.style.backgroundColor = "yellow";
+  element.style.color = "black";
+}
